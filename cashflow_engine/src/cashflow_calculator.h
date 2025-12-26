310a1f0336525cdd03887b8bcf5c0fa26403b3ad
Kumar Akashdeep <kadeep47@gmail.com>
2025-12-26 19:30:00 +0530

    feat: complete implementation of cashflow_calculator.h

--- cashflow_engine/src/cashflow_calculator.h
+++ cashflow_engine/src/cashflow_calculator.h
@@ -9,3 +9,14 @@ struct Cashflow {
     std::string date;
     double principal_amount;
     double interest_amount;
+    double total_amount;
+};
+
+class CashflowCalculator {
+public:
+    static std::vector<Cashflow> calculate_loan_cashflows(const Instrument& inst);
+    static std::vector<Cashflow> calculate_bond_cashflows(const Instrument& inst);
+    static std::vector<Cashflow> calculate_deposit_cashflows(const Instrument& inst);
+    
+    static std::vector<Cashflow> calculate_all(const std::vector<Instrument>& instruments);
+};
