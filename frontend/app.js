8b1179e26d0fdd479fd839006090e02ce8f9adfa
Kumar Akashdeep <kadeep47@gmail.com>
2026-03-19 05:30:00 +0530

    feat: add scaffold for app.js

---
+++ frontend/app.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // Navigation
+    const navBtns = document.querySelectorAll('.nav-btn');
+    const sections = document.querySelectorAll('.view-section');
+
+    navBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            navBtns.forEach(b => b.classList.remove('active'));
+            sections.forEach(s => s.classList.remove('active'));
+            
+            btn.classList.add('active');
+            document.getElementById(btn.dataset.target).classList.add('active');
+        });
+    });
